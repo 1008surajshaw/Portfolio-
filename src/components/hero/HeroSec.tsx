37786335ac77cd5646e744e3130f62f37d0c47ba
suraj <iamsuraj3000@gmail.com>
2023-11-21 23:15:17 +0530

    chaged cv

--- src/components/hero/HeroSec.tsx
+++ src/components/hero/HeroSec.tsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from "react";
 import {motion,useAnimation} from "framer-motion"
 import uim from "../../assets/userAsset/avatar-smile.webp"
-import CV from "../../assets/SurajC.V.pdf"
+import CV from "../../assets/SurajCV_.pdf"
 import {Element} from "react-scroll"
 import bckimg from "../../assets/userAsset/blob vector.png"
 
